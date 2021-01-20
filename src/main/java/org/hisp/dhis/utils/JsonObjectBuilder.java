4a1182dd822d0bf4ef44edd48f38e8a67fd75f85
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-01-20 12:08:23 +0100

    feat: adds json object builder

---
+++ src/main/java/org/hisp/dhis/utils/JsonObjectBuilder.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2004-2020, University of Oslo
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * Neither the name of the HISP project nor the names of its contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.utils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * @author Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
+ */
+public class JsonObjectBuilder
+{
+    private JsonObject jsonObject;
+
+    public JsonObjectBuilder()
+    {
+        jsonObject = new JsonObject();
+    }
+
+    public JsonObjectBuilder( JsonObject jsonObject )
+    {
+        this.jsonObject = jsonObject;
+    }
+
+    public static JsonObjectBuilder jsonObject()
+    {
+        return new JsonObjectBuilder();
+    }
+
+    public static JsonObjectBuilder jsonObject( JsonObject jsonObject )
+    {
+        return new JsonObjectBuilder( jsonObject );
+    }
+
+
+    public JsonObjectBuilder addProperty( String property, String value )
+    {
+        jsonObject.addProperty( property, value );
+
+        return this;
+    }
+
+    public JsonObjectBuilder addObject( String property, JsonObjectBuilder obj )
+    {
+        jsonObject.add( property, obj.build() );
+
+        return this;
+    }
+
+    public JsonObjectBuilder addArray( String property, JsonObject... objects) {
+        JsonArray array = new JsonArray();
+        for ( int i = 0; i < objects.length; i++ )
+        {
+            array.add( objects[i] );
+        }
+
+        jsonObject.add( property, array);
+
+        return this;
+    }
+
+
+    public JsonObjectBuilder addOrAppendToArray( String property, JsonObject... objects) {
+        if (jsonObject.has(property) ) {
+            for ( int i = 0; i < objects.length; i++ )
+            {
+                jsonObject.getAsJsonArray(property).add( objects[i] );
+            }
+        }
+
+        else {
+            addArray( property, objects );
+        }
+
+        return this;
+    }
+
+    public JsonObject wrapIntoArray( String arrayName )
+    {
+        JsonArray array = new JsonArray();
+
+        JsonObject newObj = new JsonObject();
+
+        array.add( jsonObject );
+
+        newObj.add( arrayName, array );
+
+        return newObj;
+    }
+
+    public JsonObject build()
+    {
+        return this.jsonObject;
+    }
+}
